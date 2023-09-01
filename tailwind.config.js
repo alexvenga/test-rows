480a4b2371f9e16bae57ba8df272175aea25f11d
alexvenga <bubnov.alexey@gmail.com>
2023-09-02 01:17:16 +0300

    Init npm

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: false, // or 'media' or 'class'
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
